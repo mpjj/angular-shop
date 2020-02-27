a56310c45d59b1460f91908b070e050d770f52af
mpjj <michael.p.j.johnson@gmail.com>
2020-02-27 16:58:12 +0000

    Added EventEmitter for sale notification.

--- src/app/product-alert/product-alert.component.ts
+++ src/app/product-alert/product-alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alert',
@@ -7,7 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProductAlertComponent implements OnInit {
   @Input() product;
-  
+  @Output() notify = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
